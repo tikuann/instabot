dfab3f38e515c1e1b3458512c426d7d0ef1403cd
tikuann <tikuan_0219@yahoo.co.jp>
2023-10-10 01:33:54 +0900

    ログイン画面の作成

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import login
 
 urlpatterns = [
     path('test/', views.test, name='test'),
+    path('login/', views.login, name='login'),
 ]
--- myapp/views/__init__.py
+++ myapp/views/__init__.py
@@ -1,2 +1,3 @@
 # myapp/views/__init__.py
 from .test_view import test
+from .views import login
